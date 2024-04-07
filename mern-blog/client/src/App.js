ea1bfc882ea7fd7c5f1091e689d5b930ebd781de
RA <75052820+arraayyy@users.noreply.github.com>
2024-04-07 15:16:57 +0800

    navbar

--- mern-blog/client/src/App.js
+++ mern-blog/client/src/App.js
@@ -1,14 +1,18 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+
 // Pages
 import Home from './pages/Home';
 import About from './pages/About';
 import Articles from './pages/Articles';
 import ArticlesList from './pages/ArticlesList';
 
+// Components
+import Navbar from './components/Navbar';
 
 function App() {
   return (
     <Router>
+      <Navbar />
       <div className="max-w-screen-md mx-auto p-20">
         <Routes>
           <Route path="/" element={<Home />} />
