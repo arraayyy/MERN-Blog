d8c5e34b997185ecb561711329d1a8783f9155a3
RA <75052820+arraayyy@users.noreply.github.com>
2024-04-09 13:10:39 +0800

    components and 404 page

--- mern-blog/client/src/App.js
+++ mern-blog/client/src/App.js
@@ -3,8 +3,9 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 // Pages
 import Home from './pages/Home';
 import About from './pages/About';
-import Articles from './pages/Articles';
+import Article from './pages/Article';
 import ArticlesList from './pages/ArticlesList';
+import NotFound from './pages/NotFound';
 
 // Components
 import Navbar from './components/Navbar';
@@ -18,7 +19,8 @@ function App() {
           <Route path="/" element={<Home />} />
           <Route path="/about" element={<About />} />
           <Route path="/articles-list" element={<ArticlesList />} /> 
-          <Route path="/article/:name" element={<Articles />} />
+          <Route path="/article/:name" element={<Article />} />
+          <Route path="*" element={<NotFound />} />
         </Routes>
       </div>
     </Router>
