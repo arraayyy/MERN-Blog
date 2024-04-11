104eae1be2d68b97fb5f2378bb551cceb6c06a72
RA <75052820+arraayyy@users.noreply.github.com>
2024-04-11 21:06:32 +0800

    post request error

--- mern-blog/server.js
+++ mern-blog/server.js
@@ -2,8 +2,16 @@ const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 8000;
 
+// Initialize Middleware
+
+app.use(express.json({ extended: false }));
+
 app.get("/", (req, res) => {                                                          
   res.send("Hello World");
+});
+
+app.post("/", (req, res) => {
+    res.send(`Hello ${req.body.name}`);
 });
 
 app.listen(PORT, () => {
